8e5dfec180c622469fe7ca8455d9bf306a20faee
hungle132 <hungiscoolguy@yahoo.com>
2020-02-09 23:38:25 -0800

    main menu

--- prototype/usermenu.hpp
+++ prototype/usermenu.hpp
@@ -2,13 +2,16 @@
 #define __USERMENU_HPP__
 
 #include "menu.hpp"
+#include <unistd.h>
+#include <cstdlib>
+#include <stdio.h>
 #include <vector>
 #include <string>
 
 class Usermenu : public Menu {
 	protected:
-	vector<string>inputs;
-	string inputs;
+	vector<string>commands;
+	string input;
 
 	public:
 	
@@ -16,10 +19,14 @@ class Usermenu : public Menu {
 	
 	void run() {
 		
-
-
-
+	while(input != "exit"){
+	cout << "$ ";
+	getline(cin,input); 
+	
 	}
+	
+
+}
 };
 
 #endif //__USERMENU__HPP__
