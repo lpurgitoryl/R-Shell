83be7736996117aa972f1da115a5a729cbbb8529
hungle132 <hungiscoolguy@yahoo.com>
2020-02-09 21:31:28 -0800

    menu

---
+++ prototype/menu.hpp
@@ -0,0 +1,18 @@
+#ifndef __menu_hpp__
+#define __menu_hpp__
+
+#include <string>
+#include <vector>
+
+
+
+class Menu {
+	public:
+
+	Menu(){};
+
+	virtual void run(){};
+};
+
+
+#endif //__menu_hpp__	
---
+++ prototype/usermenu.hpp
@@ -0,0 +1,25 @@
+#ifndef __USERMENU_HPP__
+#define __USERMENU_HPP__
+
+#include "menu.hpp"
+#include <vector>
+#include <string>
+
+class Usermenu : public Menu {
+	protected:
+	vector<string>inputs;
+	string inputs;
+
+	public:
+	
+	Usermenu();
+	
+	void run() {
+		
+
+
+
+	}
+};
+
+#endif //__USERMENU__HPP__
