34d8385e4423a5eb34ab35866fcf6ec345eec53d
hungle132 <hungiscoolguy@yahoo.com>
2020-02-10 02:08:02 -0800

    Abstract Base Class

---
+++ header/Basemenu.h
@@ -0,0 +1,20 @@
+#ifndef __Basemenu__
+#define __Basemenu__
+
+#include <iostream>
+
+Class Basemenu{
+
+
+	public:
+
+	Basemenu(){};
+
+	virtual void run() = 0;
+	virtual bool execute() = 0;
+
+
+};
+
+
+#endif //__Basemenu__
