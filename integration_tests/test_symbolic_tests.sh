438825f01933381b2ceb0e4f67e5abcb0b69739e
lpurgitoryl <ngarcia201@gmail.com>
2020-02-28 20:56:17 -0800

    test exe

--- integration_tests/test_symbolic_tests.sh
+++ integration_tests/test_symbolic_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-INPUTS = ("[ -e src/main.cpp ]" "[ -d header ]" "[ -f README.md ]")
+INPUTS = ("[ -e src/main.cpp ]" "[ -d header ]" "[ -f README.md ]" "[ header/]")
 
+./../rshell ${INPUTS[4]} 
 ./../rshell ${INPUTS[2]}
 ./../rshell ${INPUTS[1]}
 ./../rshell ${INPUTS[0]} 
